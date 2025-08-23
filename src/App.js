662bc77bbc7b1dca5d03a77b699b48d6420da098
mundadaVinay <vmundada2004@gmail.com>
2025-08-24 02:28:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import HiveMindDashboard from './HiveMindDashboard';
+import React, { useState } from "react";
+import HiveMindDashboard from "./HiveMindDashboard";
+import Login from "./Login"; // Fancy login component
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <HiveMindDashboard />
-    </div>
-  );
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  // Render login if user not logged in
+  if (!user) {
+    return <Login onLogin={setUser} />;
+  }
+
+  // Once logged in, render dashboard
+  return <HiveMindDashboard user={user} />;
 }
-
-export default App;
